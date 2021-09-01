d3ae44869cbaf5b41a566f8e7c8fe28feded42f3
sam-thewise <caliginosity.insanity@gmail.com>
2021-09-01 16:34:35 +1200

    Added access permissions
    Only the contract owner should be able to withdraw and lock funds

--- contracts/Locker.sol
+++ contracts/Locker.sol
@@ -7,8 +7,9 @@ pragma solidity ^0.8.0;
 import { IERC20 } from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 import { SafeERC20 } from '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
 import { ReentrancyGuard } from '@openzeppelin/contracts/security/ReentrancyGuard.sol';
+import { Ownable } from '@openzeppelin/contracts/access/Ownable.sol';
 
-contract AssetLocker is ReentrancyGuard {
+contract AssetLocker is ReentrancyGuard, Ownable {
     using SafeERC20 for IERC20;
 
     event AssetLocked (
@@ -33,7 +34,7 @@ contract AssetLocker is ReentrancyGuard {
 
     mapping (bytes32 => LockedAsset) public lockedAssets;
 
-    function lockAsset (IERC20 asset, uint256 lockDays) external nonReentrant {
+    function lockAsset (IERC20 asset, uint256 lockDays) external nonReentrant onlyOwner {
         bytes32 identifier = getIdentifier(asset);
         uint256 amount = asset.balanceOf(msg.sender);
 
@@ -50,7 +51,7 @@ contract AssetLocker is ReentrancyGuard {
         emit AssetLocked(msg.sender, address(asset), amount, lockDays, block.timestamp);
     }
 
-    function withdrawAsset (IERC20 asset) external nonReentrant {
+    function withdrawAsset (IERC20 asset) external nonReentrant onlyOwner {
         bytes32 identifier = getIdentifier(asset);
 
         LockedAsset storage lockedAsset = lockedAssets[identifier];
